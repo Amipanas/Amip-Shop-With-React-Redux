ceb7a0f95bcc9344d9f59b9d2430b3fe7aa5e13a
Amipanas <nasiriparsa3@gmail.com>
2022-11-05 20:22:30 +0330

    Amip E-commerce created with "React" , "Redux" , "React Router Dom" and "Axios".

---
+++ amip-shop/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router ,Routes ,Route } from "react-router-dom";
+import ProductListing from './components/ProductListing';
+import Header from './components/Header';
+import './App.css';
+import ProductDetail from './components/ProductDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ amip-shop/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="ui menu">
+      <div className="ui container center">
+      <img src="logo.png" style={{'width':'260px', 'height':'60px', 'margin':'15px 5px 15px 0px'}}/>
+        {/* <h2>Amip Shop</h2> */}
+      </div>
+    </div>
+  )
+}
+ 
+export default Header;
